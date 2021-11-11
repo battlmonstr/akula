60972526be3a8203b066ee195872664f1f97e755
battlmonstr <battlmonstr@users.noreply.github.com>
2021-11-11 20:01:54 +0100

    Downloader: refactor to use the global chain config.

--- src/sentry/chain_config.rs
+++ src/sentry/chain_config.rs
@@ -1,23 +1,66 @@
 use super::chain_id::ChainId;
-use serde::Deserialize;
+use crate::{models::BlockNumber, state::genesis::GenesisData};
+use hex_literal::hex;
 use std::collections::HashMap;
 
-pub struct ChainsConfig(pub HashMap<String, ChainConfig>);
+pub struct ChainsConfig(HashMap<String, ChainConfig>);
 
-#[derive(Deserialize, Clone)]
+#[derive(Clone)]
 pub struct ChainConfig {
-    pub id: ChainId,
-    #[serde(rename = "genesis")]
-    pub genesis_block_hash: ethereum_types::H256,
-    #[serde(rename = "fork_blocks")]
-    pub fork_block_numbers: Vec<u64>,
+    genesis: &'static GenesisData,
+    genesis_block_hash: ethereum_types::H256,
+}
+
+impl ChainConfig {
+    fn new(genesis: &'static GenesisData, genesis_block_hash: ethereum_types::H256) -> Self {
+        // TODO: calculate hash from GenesisData
+        // let genesis_header = genesis.header(&genesis.initial_state());
+        // let genesis_block_hash = genesis_header.hash();
+        Self {
+            genesis,
+            genesis_block_hash,
+        }
+    }
+
+    pub fn id(&self) -> ChainId {
+        self.genesis.config.chain_id
+    }
+
+    pub fn genesis_block_hash(&self) -> ethereum_types::H256 {
+        self.genesis_block_hash
+    }
+
+    pub fn fork_block_numbers(&self) -> Vec<BlockNumber> {
+        self.genesis.config.gather_forks().iter().cloned().collect()
+    }
 }
 
 impl ChainsConfig {
     pub fn new() -> anyhow::Result<Self> {
-        let config_text = include_str!("chain_config.toml");
-        let configs: HashMap<String, ChainConfig> = toml::from_str(config_text)?;
+        let mut configs = HashMap::<String, ChainConfig>::new();
+        configs.insert(
+            String::from("mainnet"),
+            ChainConfig::new(
+                &crate::res::genesis::MAINNET,
+                ethereum_types::H256(hex!(
+                    "d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3"
+                )),
+            ),
+        );
+        configs.insert(
+            String::from("ropsten"),
+            ChainConfig::new(
+                &crate::res::genesis::ROPSTEN,
+                ethereum_types::H256(hex!(
+                    "41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d"
+                )),
+            ),
+        );
         Ok(ChainsConfig(configs))
+    }
+
+    pub fn get(&self, chain_name: &str) -> Option<&ChainConfig> {
+        self.0.get(chain_name)
     }
 }
 
--- src/state/genesis.rs
+++ src/state/genesis.rs
@@ -1,4 +1,3 @@
-use super::*;
 use crate::{
     kv::tables::{self, CumulativeData, PlainStateFusedValue},
     models::{ChainConfig, *},
@@ -34,6 +33,46 @@ pub struct GenesisData {
     pub timestamp: u64,
 }
 
+impl GenesisData {
+    pub fn initial_state(&self) -> InMemoryState {
+        let mut state_buffer = InMemoryState::new();
+        // Allocate accounts
+        for (address, account) in &self.alloc {
+            let current_account = Account {
+                balance: account.balance,
+                ..Default::default()
+            };
+            state_buffer.update_account_sync(*address, None, Some(current_account));
+        }
+        state_buffer
+    }
+
+    pub fn header(&self, initial_state: &InMemoryState) -> BlockHeader {
+        let genesis = self;
+        let state_root = initial_state.state_root_hash();
+
+        BlockHeader {
+            parent_hash: H256::zero(),
+            beneficiary: genesis.coinbase,
+            state_root,
+            logs_bloom: Bloom::zero(),
+            difficulty: genesis.difficulty,
+            number: BlockNumber(0),
+            gas_limit: genesis.gas_limit,
+            gas_used: 0,
+            timestamp: genesis.timestamp,
+            extra_data: genesis.extra_data.clone(),
+            mix_hash: genesis.mix_hash,
+            nonce: genesis.nonce,
+            base_fee_per_gas: None,
+
+            receipts_root: EMPTY_ROOT,
+            ommers_hash: EMPTY_LIST_HASH,
+            transactions_root: EMPTY_ROOT,
+        }
+    }
+}
+
 pub async fn initialize_genesis<'db, Tx>(txn: &Tx, genesis: GenesisData) -> anyhow::Result<bool>
 where
     Tx: MutableTransaction<'db>,
@@ -46,22 +85,7 @@ where
         return Ok(false);
     }
 
-    let mut state_buffer = InMemoryState::new();
-    // Allocate accounts
-    for (address, account) in genesis.alloc {
-        let balance = account.balance;
-
-        state_buffer
-            .update_account(
-                address,
-                None,
-                Some(Account {
-                    balance,
-                    ..Default::default()
-                }),
-            )
-            .await?;
-    }
+    let state_buffer = genesis.initial_state();
 
     // Write allocations to db - no changes only accounts
     let mut state_table = txn.mutable_cursor(&tables::PlainState).await?;
@@ -75,27 +99,7 @@ where
             .await?;
     }
 
-    let state_root = state_buffer.state_root_hash();
-
-    let header = BlockHeader {
-        parent_hash: H256::zero(),
-        beneficiary: genesis.coinbase,
-        state_root,
-        logs_bloom: Bloom::zero(),
-        difficulty: genesis.difficulty,
-        number: BlockNumber(0),
-        gas_limit: genesis.gas_limit,
-        gas_used: 0,
-        timestamp: genesis.timestamp,
-        extra_data: genesis.extra_data,
-        mix_hash: genesis.mix_hash,
-        nonce: genesis.nonce,
-        base_fee_per_gas: None,
-
-        receipts_root: EMPTY_ROOT,
-        ommers_hash: EMPTY_LIST_HASH,
-        transactions_root: EMPTY_ROOT,
-    };
+    let header = genesis.header(&state_buffer);
     let block_hash = header.hash();
 
     txn.set(&tables::Header, ((0.into(), block_hash), header.clone()))
@@ -167,6 +171,27 @@ mod tests {
         assert_eq!(
             genesis_hash,
             hex!("d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3").into()
+        );
+    }
+
+    #[test]
+    fn mainnet_genesis_hash() {
+        let genesis = &crate::res::genesis::MAINNET;
+        let genesis_hash = genesis.header(&genesis.initial_state()).hash();
+        assert_eq!(
+            genesis_hash,
+            hex!("d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3").into()
+        );
+    }
+
+    // TODO: fix
+    //#[test]
+    fn ropsten_genesis_hash() {
+        let genesis = &crate::res::genesis::ROPSTEN;
+        let genesis_hash = genesis.header(&genesis.initial_state()).hash();
+        assert_eq!(
+            genesis_hash,
+            hex!("41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d").into()
         );
     }
 }
