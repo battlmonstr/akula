86900b24b9796abc0117ba93be4115e162dc2573
battlmonstr <battlmonstr@users.noreply.github.com>
2021-11-16 15:25:52 +0100

    Downloader: verify_slice_difficulties.

--- src/sentry/chain_config.rs
+++ src/sentry/chain_config.rs
@@ -29,6 +29,14 @@ impl ChainConfig {
         self.chain_spec.params.chain_id
     }
 
+    pub fn chain_name(&self) -> String {
+        self.chain_spec.name.clone()
+    }
+
+    pub fn chain_spec(&self) -> &ChainSpec {
+        &self.chain_spec
+    }
+
     pub fn genesis_block_hash(&self) -> ethereum_types::H256 {
         self.genesis_block_hash
     }
